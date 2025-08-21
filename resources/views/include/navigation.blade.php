a763ba4ffd3e6e6179fd5c43c613860b47298a6c
vk-mithlesh <mithlesh.jha@vyaparkranti.in>
2025-08-21 15:26:33 +0530

    social media links removal

--- resources/views/include/navigation.blade.php
+++ resources/views/include/navigation.blade.php
@@ -9,7 +9,7 @@
             <li><a href="mailto:{!! $email_2??"support@communicationrn.com" !!}"><i class="fa fa-envelope"></i>&nbsp;<span>{!! $email_2??"info@3dprecission.com" !!}</span></a></li>
             <li><a href="tel:+91{!! isset($contact_us_contact_number)?str_replace(" ","",$contact_us_contact_number):"+919266747031" !!}"><i class="fa fa-phone"></i>&nbsp;<span>{!! $contact_us_contact_number??"+91 926 674 7031" !!}</span></a></li>
         </ul>
-        <div class="gtranslate_wrapper"></div>
+        {{-- <div class="gtranslate_wrapper"></div> --}}
     </div>
 </div> 
 <!-- Header section Start -->
@@ -50,12 +50,12 @@
     </li>
 </ul>
 </div>
-            <ul class="social-media">
+            {{-- <ul class="social-media">
                 <li><a href="{!! $facebook_link ?? 'https://www.facebook.com/3dprecission' !!}" aria-label="Read more about 3dprecission  facebook"><i class="fa-brands fa-facebook"></i></a></li>
                 <li><a href="{!! $linkedin_link ?? '/' !!}" aria-label="Read more about 3dprecission  Linkedin"><i class="fab fa-linkedin"></i></a></li>
                 <li><a href="{!! $instagram_link ?? 'https://www.instagram.com/adiyogi_global/' !!}" aria-label="Read more about 3dprecission  Instagram"><i class="fa-brands fa-instagram"></i></a></li>
                 <li><a href="{!! $youtube_link ?? 'https://www.youtube.com/@3dprecission' !!}" aria-label="Read more about 3dprecission  Youtube"><i class="fa-brands fa-youtube"></i></a></li>
-            </ul>
+            </ul> --}}
         </div>
     </div>
 </header>
